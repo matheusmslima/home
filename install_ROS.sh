c82fa4b2d5d9c1a128e237ffe805f4b2802c6f94
Matheus Lima <matheusmslima@gmail.com>
2021-06-22 01:24:31 -0300

    update of install_ROS

--- install_ROS.sh
+++ install_ROS.sh
@@ -1,6 +1,6 @@
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu focal main" > /etc/apt/sources.list.d/ros-latest.list'
 
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+# curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
 
